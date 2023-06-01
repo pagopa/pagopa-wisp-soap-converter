5734004d5e3254637e24f732f0e6e5992cd6652d
Ignazio Bricco <ignazio.bricco@gmail.com>
2023-06-01 14:51:28 +0200

    NOD-278: Salvare su storage gli xml rendicontazioni

--- fdr/src/main/scala/eu/sia/pagopa/common/util/azurestorageblob/AzureStorageBlobClient.scala
+++ fdr/src/main/scala/eu/sia/pagopa/common/util/azurestorageblob/AzureStorageBlobClient.scala
@@ -2,6 +2,8 @@ package eu.sia.pagopa.common.util.azurestorageblob
 
 import akka.actor.ActorSystem
 import akka.dispatch.MessageDispatcher
+import com.azure.core.credential.TokenCredential
+import com.azure.core.util.BinaryData
 import com.azure.identity.DefaultAzureCredentialBuilder
 import com.azure.storage.blob.BlobServiceClientBuilder
 import eu.sia.pagopa.common.util.NodoLogger
@@ -16,27 +18,28 @@ object AzureStorageBlobClient {
     val azureStorageBlobEnabled = system.settings.config.getBoolean("azure-storage-blob.enabled")
 
     if( azureStorageBlobEnabled ) {
-      val azureStorageBlobContainerName = system.settings.config.getString("azure-storage-blob.containerName")
-      val azureStorageBlobContainerEndpoint = system.settings.config.getString("azure-storage-blob.endpoint")
-      val defaultCredential = new DefaultAzureCredentialBuilder().build()
+      val containerName = system.settings.config.getString("azure-storage-blob.container-name")
+      val connectionString = system.settings.config.getString("azure-storage-blob.connection-string")
+      val defaultCredential: TokenCredential = new DefaultAzureCredentialBuilder().build()
 
       val blobServiceClient = new BlobServiceClientBuilder()
-        .endpoint(azureStorageBlobContainerEndpoint)
         .credential(defaultCredential)
+        .connectionString(connectionString)
         .buildClient()
 
-      val blobClient = blobServiceClient.getBlobContainerClient(azureStorageBlobContainerName)
+      val containerClient = blobServiceClient.getBlobContainerClient(containerName)
 
       (fileName: String, inputStream: InputStream, log: NodoLogger) => {
         val executionContext: MessageDispatcher = system.dispatchers.lookup("eventhub-dispatcher")
-        Future(blobClient.getBlobClient(fileName).upload(inputStream))(executionContext) recoverWith { case e: Throwable =>
-          log.error(e, "Error azure-storage-blob")
-          Future.failed(e)
+        Future(containerClient.getBlobClient(fileName).upload(BinaryData.fromStream(inputStream)))(executionContext) recoverWith {
+          case e: Throwable =>
+            log.error(e, "Error calling azure-storage-blob")
+            Future.failed(e)
         }
       }
     } else {
       (fileName: String, inputStream: InputStream, log: NodoLogger) => {
-        log.debug("CONFIGURATION_KEYS [azure-storage-blob.enabled]=false. Not storage blob")
+        log.debug("config-app [azure-storage-blob.enabled]=false. Not enable to storage blob")
         Future.successful(())
       }
     }
