221b10cb2c5cafd3cb7662d46bd83fe590a51155
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-08-05 17:51:11 +0200

    [PAGOPA-1934] feat: NodoChiediCopiaRTActorPerRequest

---
+++ wispsoapconverter/src/main/scala/it/gov/pagopa/actors/NodoChiediCopiaRTActor.scala
@@ -0,0 +1,28 @@
+package it.gov.pagopa.actors
+
+import akka.actor.ActorRef
+import akka.http.scaladsl.model.StatusCodes
+import it.gov.pagopa.ActorProps
+import it.gov.pagopa.actors.response.NodoInviaRPTResponse
+import it.gov.pagopa.common.actor.PerRequestActor
+import it.gov.pagopa.common.enums._
+import it.gov.pagopa.common.exception
+import it.gov.pagopa.common.exception.{DigitPaErrorCodes, DigitPaException}
+import it.gov.pagopa.common.message._
+import it.gov.pagopa.common.repo.{CosmosPrimitive, CosmosRepository}
+import it.gov.pagopa.common.rpt.split.RptFlow
+import it.gov.pagopa.common.util._
+import it.gov.pagopa.common.util.azure.cosmos.{CategoriaEvento, Componente, Esito, SottoTipoEvento}
+import it.gov.pagopa.config.Channel
+import it.gov.pagopa.exception.{RptFaultBeanException, WorkflowExceptionErrorCodes}
+import org.slf4j.MDC
+import scalaxbmodel.nodoperpa.{IntestazionePPT, NodoInviaRPTRisposta}
+import scalaxbmodel.paginf.CtRichiestaPagamentoTelematico
+
+import java.time.Instant
+import java.util.Base64
+import scala.concurrent.Future
+
+case class NodoChiediCopiaRTActorPerRequest(cosmosRepository: CosmosRepository, actorProps: ActorProps) extends PerRequestActor with NodoInviaRPTResponse with RptFlow with ReUtil {
+
+}
