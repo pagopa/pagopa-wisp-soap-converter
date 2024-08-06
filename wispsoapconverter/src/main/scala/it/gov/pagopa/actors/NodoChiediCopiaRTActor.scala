067c0ba03ed5f37f8ac407b2e838522f85937ad8
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-08-06 11:46:18 +0200

    [PAGOPA-1934] feat: NodoChiediCopiaRTActorPerRequest

--- wispsoapconverter/src/main/scala/it/gov/pagopa/actors/NodoChiediCopiaRTActor.scala
+++ wispsoapconverter/src/main/scala/it/gov/pagopa/actors/NodoChiediCopiaRTActor.scala
@@ -3,20 +3,16 @@ package it.gov.pagopa.actors
 import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
 import it.gov.pagopa.ActorProps
-import it.gov.pagopa.actors.response.NodoInviaRPTResponse
 import it.gov.pagopa.common.actor.PerRequestActor
 import it.gov.pagopa.common.enums._
 import it.gov.pagopa.common.exception
 import it.gov.pagopa.common.exception.{DigitPaErrorCodes, DigitPaException}
 import it.gov.pagopa.common.message._
 import it.gov.pagopa.common.repo.{CosmosPrimitive, CosmosRepository}
-import it.gov.pagopa.common.rpt.split.RptFlow
 import it.gov.pagopa.common.util._
 import it.gov.pagopa.common.util.azure.cosmos.{CategoriaEvento, Componente, Esito, SottoTipoEvento}
-import it.gov.pagopa.config.Channel
-import it.gov.pagopa.exception.{RptFaultBeanException, WorkflowExceptionErrorCodes}
+import it.gov.pagopa.exception.{WorkflowExceptionErrorCodes}
 import org.slf4j.MDC
-import scalaxbmodel.nodoperpa.{IntestazionePPT, NodoInviaRPTRisposta}
 import scalaxbmodel.paginf.CtRichiestaPagamentoTelematico
 
 import java.time.Instant
@@ -26,6 +22,7 @@ import scala.concurrent.Future
 case class NodoChiediCopiaRTActorPerRequest(cosmosRepository: CosmosRepository, actorProps: ActorProps) extends PerRequestActor {
   var req: SoapRequest = _
   var replyTo: ActorRef = _
+
   override def receive: Receive = {
     case soapRequest: SoapRequest =>
       req = soapRequest
--- wispsoapconverter/src/main/scala/it/gov/pagopa/common/util/Primitive.scala
+++ wispsoapconverter/src/main/scala/it/gov/pagopa/common/util/Primitive.scala
@@ -2,12 +2,15 @@ package it.gov.pagopa.common.util
 
 import it.gov.pagopa.actors.{NodoInviaCarrelloRPTActorPerRequest, NodoInviaRPTActorPerRequest}
 import it.gov.pagopa.common.actor.PerRequestActor
+import scalaxbmodel.nodoperpa.NodoChiediCopiaRT
+import it.gov.pagopa.actors.NodoChiediCopiaRTActorPerRequest
 
 object Primitive {
 
   val soap: Map[String, (String, Boolean => Class[_ <: PerRequestActor])] = Map(
     "nodoInviaRPT" -> ("Body/_/identificativoIntermediarioPA", _ => classOf[NodoInviaRPTActorPerRequest]),
-    "nodoInviaCarrelloRPT" -> ("Header/_/identificativoStazioneIntermediarioPA", _ => classOf[NodoInviaCarrelloRPTActorPerRequest])
+    "nodoInviaCarrelloRPT" -> ("Header/_/identificativoStazioneIntermediarioPA", _ => classOf[NodoInviaCarrelloRPTActorPerRequest]),
+    "nodoChiediCopiaRT" -> ("Header/_/identificativoStazioneIntermediarioPA", _ => classOf[NodoChiediCopiaRTActorPerRequest])
   )
 
   val allPrimitives = Primitive.soap
