aa685734f0a9dd2edfbb2130e682aca0de4f94fb
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-08-28 10:15:54 +0200

    PAGOPA-2062 removing commented code

--- wispsoapconverter/src/main/scala/it/gov/pagopa/common/util/RPTUtil.scala
+++ wispsoapconverter/src/main/scala/it/gov/pagopa/common/util/RPTUtil.scala
@@ -11,46 +11,13 @@ import scala.xml.XML
 
 object RPTUtil {
 
-//  def getAdapterEcommerceUri(uri: String, req:SoapRequest, int:IntestazioneCarrelloPPT): String = {
-////    s"${uri}${getUniqueKey(req, int)}"
-//    uri.replaceAll("REPLACE", getUniqueKey(req, int))
-//  }
   def getAdapterEcommerceUri(uri: String, req:SoapRequest): String = {
 //    s"${uri}${getUniqueKey(req, int)}"
     uri.replaceAll("REPLACE", req.sessionId)
   }
 
-//  def getUniqueKey(req:SoapRequest, int:IntestazioneCarrelloPPT) = {
-//    s"${req.sessionId}"
-//  }
-//  def getUniqueKey(req:SoapRequest, int:IntestazionePPT) = {
-//    s"${req.sessionId}"
-//  }
-
   def getRptByBase64Binary(rptEncoded: Base64Binary): Try[CtRichiestaPagamentoTelematico] = {
     Success(scalaxb.fromXML[CtRichiestaPagamentoTelematico](XML.loadString(StringBase64Binary.decodeBase64(rptEncoded))))
   }
-
-//  def Carrello2Str(header:scalaxbmodel.nodoperpa.IntestazioneCarrelloPPT,body: scalaxbmodel.nodoperpa.NodoInviaCarrelloRPT): Try[(String,String)] = {
-//      val h = Try(scalaxb.toXML[scalaxbmodel.nodoperpa.IntestazioneCarrelloPPT](header, "IntestazioneCarrelloPPT", scalaxbmodel.paginf.defaultScope))
-//      val hh = h.map(v => transform(v.toString))
-//      val b = Try(scalaxb.toXML[scalaxbmodel.nodoperpa.NodoInviaCarrelloRPT](body, "NodoInviaCarrelloRPT", scalaxbmodel.paginf.defaultScope))
-//      val bb = b.map(v => transform(v.toString))
-//    for {
-//      header <- hh
-//      body <- bb
-//    } yield (header,body)
-//  }
-//
-//  def RPT2Str(header:scalaxbmodel.nodoperpa.IntestazionePPT,body: scalaxbmodel.nodoperpa.NodoInviaRPT): Try[(String,String)] = {
-//    val h = Try(scalaxb.toXML[scalaxbmodel.nodoperpa.IntestazionePPT](header, "IntestazionePPT", scalaxbmodel.paginf.defaultScope))
-//    val hh = h.map(v => transform(v.toString))
-//    val b = Try(scalaxb.toXML[scalaxbmodel.nodoperpa.NodoInviaRPT](body, "NodoInviaRPT", scalaxbmodel.paginf.defaultScope))
-//    val bb = b.map(v => transform(v.toString))
-//    for {
-//      header <- hh
-//      body <- bb
-//    } yield (header,body)
-//  }
 
 }
